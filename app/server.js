717e26fad107f01e17d1687b07eb932a9698f7d4
TJ Egan <twegan@us.ibm.com>
2017-01-16 12:31:56 -0600

    add hbs for templating

--- app/server.js
+++ app/server.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express); // eslint-disable-line no-underscore-dangle
+
+app.set('views', path.resolve(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'dist')));
 
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
 app.get('/', (req, res) => {
-  res.sendFile('/index.html');
+  res.render('home');
 });
 
 module.exports = app;
