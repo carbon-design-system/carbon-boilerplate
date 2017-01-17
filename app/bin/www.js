fdc226703195c5d21ddb4b436edc0b3815bb3a5f
TJ Egan <twegan@us.ibm.com>
2017-01-17 11:02:56 -0600

    move theme-switcher, add sass-lint

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 const app = require('../server');
+
 const port = process.env.PORT || 7777;
 
 app.listen(port, (err) => {
   if (err) {
-    console.log(err);
+    console.log(err); // eslint-disable-line
     return;
   }
 
-  console.log(`\nServer running\n`);
+  console.log('\nServer running\n'); // eslint-disable-line
 });
